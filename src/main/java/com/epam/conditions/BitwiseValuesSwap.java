5be567ac5b46ff4dfbb061ca9e0875cf6a0b24db
MZykin <zykininho@gmail.com>
2023-03-24 17:25:42 +0600

    feat: Conditions - Task 6. Bitwise values swap.

--- src/main/java/com/epam/conditions/BitwiseValuesSwap.java
+++ src/main/java/com/epam/conditions/BitwiseValuesSwap.java
@@ -3,6 +3,10 @@ package com.epam.conditions;
 public class BitwiseValuesSwap {
 
     public void swap(int first, int second) {
+        first = first ^ second;
+        second = first ^ second;
+        first = first ^ second;
+        System.out.println(first);
+        System.out.println(second);
     }
-
 }
