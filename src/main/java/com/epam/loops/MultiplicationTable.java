82732e126e3c615fe276a0d48ee6461bf5af98fa
MZykin <zykininho@gmail.com>
2023-03-24 17:39:28 +0600

    feat: LOOPS - Task 1. Multiplication table

--- src/main/java/com/epam/loops/MultiplicationTable.java
+++ src/main/java/com/epam/loops/MultiplicationTable.java
@@ -3,6 +3,9 @@ package com.epam.loops;
 public class MultiplicationTable {
 
     public void printTable(int numberTableToPrint) {
+        String record = "%d x %d = %d\n";
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf(record, i, numberTableToPrint, i * numberTableToPrint);
+        }
     }
-
 }
