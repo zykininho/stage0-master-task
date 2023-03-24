57f65cc984cfa53bec989f404df5258570d67c17
MZykin <zykininho@gmail.com>
2023-03-24 16:54:53 +0600

    feat: Conditions - Task 3. Days in a month.

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -1,9 +1,16 @@
 package com.epam.conditions;
 
+import java.util.Calendar;
+
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
-
+        if (year < 0 || month < 1 || month > 12) {
+            System.out.println("invalid date");
+            return;
+        }
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, 1);
+        System.out.println(calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
     }
-
 }
