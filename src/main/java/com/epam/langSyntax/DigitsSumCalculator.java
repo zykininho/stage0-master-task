edf7580256881e4780b154568a91393659a94bcc
MZykin <zykininho@gmail.com>
2023-03-24 16:27:44 +0600

    feat: Lang Syntax - Task 2. Digits sum calculator.

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,12 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        int sum = 0;
+        while (number != 0) {
+            int remainder = number % 10;
+            sum += remainder;
+            number = number / 10;
+        }
+        System.out.println(sum);
     }
-
 }
