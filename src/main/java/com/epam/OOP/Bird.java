e5b51a4b883868e3b25beef04f1a9be1f7145659
MZykin <zykininho@gmail.com>
2023-03-24 16:00:38 +0600

    feat: OOP - Task 2

--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -1,5 +1,17 @@
 package com.epam.OOP;
 
-public class Bird {
+public class Bird extends Animal {
 
+    public Bird() {
+        super("blue", 2, false);
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription().concat(" Moreover, it has 2 wings and can fly.");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Bird().getDescription());
+    }
 }
--- src/main/java/com/epam/OOP/Dog.java
+++ src/main/java/com/epam/OOP/Dog.java
@@ -1,5 +1,12 @@
 package com.epam.OOP;
 
-public class Dog {
+public class Dog extends Animal {
 
+    public Dog() {
+        super("brown", 4, true);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Dog().getDescription());
+    }
 }
