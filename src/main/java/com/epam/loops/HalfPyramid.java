08eb3e9c3585ce379fb8aefce8d93fe0a6ec05ee
MZykin <zykininho@gmail.com>
2023-03-24 18:32:33 +0600

    feat: LOOPS - Task 4. Half-Pyramid

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -3,11 +3,20 @@ package com.epam.loops;
 public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
-
+        StringBuilder pyramid = new StringBuilder();
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= (cathetusLength - i); j++) {
+                pyramid.append(" ");
+            }
+            for (int n = i; n > 0; n--) {
+                pyramid.append("*");
+            }
+            pyramid.append("\n");
+        }
+        System.out.print(pyramid);
     }
 
     public static void main(String[] args) {
         new HalfPyramid().printHalfPyramid(8);
     }
-
 }
