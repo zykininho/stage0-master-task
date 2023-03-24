7a4b59cbdf204f56aca29b4f83eab781b5f16f8e
MZykin <zykininho@gmail.com>
2023-03-24 18:17:20 +0600

    feat: LOOPS - Task 2. Pyramid

--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,11 +3,23 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
-
+        StringBuilder pyramid = new StringBuilder();
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= (cathetusLength - i); j++) {
+                pyramid.append(" ");
+            }
+            for (int n = i; n > 0; n--) {
+                pyramid.append(n);
+            }
+            for (int m = 2; m <= i; m++) {
+                pyramid.append(m);
+            }
+            pyramid.append("\n");
+        }
+        System.out.print(pyramid);
     }
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
     }
-
 }
